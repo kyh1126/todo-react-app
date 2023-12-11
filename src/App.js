dd7083242e325e3531be30e99cdf8835c5178b93
kyh1126 <kyh901126@nate.com>
2023-12-11 21:32:59 +0900

    [TIL/book] React.js, 스프링 부트, AWS로 배우는 웹 개발 101 (3-2-1. Todo 리스트 - Todo 리스트)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Todo from './Todo';
 import React, { useState } from "react";
 
 function App() {
-  const [item, setItem] = useState({
-    id: "0",
-    title: "Hello World 1",
-    done: true
-  });
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true
+    },
+  ]);
+
+  let todoItems =
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
 
   return (
     <div className="App">
-      <Todo item={item} />
+      {todoItems}
     </div>
   );
 }
