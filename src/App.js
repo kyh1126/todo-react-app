a2a6a88a302425bc3295da92412506ee4481475d
kyh1126 <kyh901126@nate.com>
2023-12-11 21:45:19 +0900

    [TIL/book] React.js, 스프링 부트, AWS로 배우는 웹 개발 101 (3-2-1. Todo 리스트 - material-ui를 이용한 디자인)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Todo from './Todo';
 import React, { useState } from "react";
+import { List, Paper } from "@mui/material";
 
 function App() {
   const [items, setItems] = useState([
@@ -16,14 +17,16 @@ function App() {
     },
   ]);
 
-  let todoItems =
-    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
-
-  return (
-    <div className="App">
-      {todoItems}
-    </div>
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id} />
+        ))}
+      </List>
+    </Paper>
   );
+  return <div className="App">{todoItems}</div>;
 }
 
 export default App;
